ad1d8e44ab559bd9ac47e27f2a4ac5116b230fcc
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2019-02-15 13:54:36 +0200

    Update ESP8266 example

--- examples/hardware/MicroPython_ESP32.py
+++ examples/hardware/MicroPython_ESP32.py
@@ -10,30 +10,52 @@ projects by simply dragging and dropping widgets.
   Social networks:            http://www.fb.com/blynkapp
                               http://twitter.com/blynk_app
 
-This example shows how to initialize your ESP32 board
+This example shows how to initialize your ESP8266/ESP32 board
 and connect it to Blynk.
 
 Don't forget to change WIFI_SSID, WIFI_AUTH and BLYNK_AUTH ;)
 """
 
 import BlynkLib
-from network import WLAN
+import network
+import machine
 
 WIFI_SSID = 'YourWiFiNetwork'
-WIFI_AUTH = (WLAN.WPA2, 'YourWiFiPassword')
+WIFI_PASS = 'YourWiFiPassword'
 
 BLYNK_AUTH = 'YourAuthToken'
 
-# Connect to WiFi
-wifi = WLAN(mode=WLAN.STA)
-wifi.connect(WIFI_SSID, auth=WIFI_AUTH)
+print("Connecting to WiFi...")
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(WIFI_SSID, WIFI_PASS)
 while not wifi.isconnected():
     pass
 
-print(wifi.ifconfig())
+print('IP: ' + wifi.ifconfig()[0])
 
-# Initialize Blynk
+print("Connecting to Blynk...")
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
-while True:
-    blynk.run()
+@blynk.ON("connected")
+def blynk_connected(ping):
+    print('Blynk ready. Ping:', ping, 'ms')
+
+def runLoop():
+    while True:
+        blynk.run()
+        machine.idle()
+
+# Run blynk in the main thread:
+runLoop()
+
+# Or, run blynk in a separate thread (unavailable for esp8266):
+#import _thread
+#_thread.stack_size(5*1024)
+#_thread.start_new_thread(runLoop, ())
+
+
+# Note:
+# Threads are currently unavailable on some devices like esp8266
+# ESP32_psRAM_LoBo has a bit different thread API:
+# _thread.start_new_thread("Blynk", runLoop, ())
